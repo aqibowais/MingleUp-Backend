7b6571a4472416bc76f923e6d8a22f02a9ef501c
Aqib Owais <owaisharoon60@gmail.com>
2025-08-22 11:03:53 +0500

    postinstall script added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import cors from "cors"
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import { connectDB } from "./lib/db.connection.js";
-
+import path from "path"
 import dotenv from "dotenv";
 import { app, server } from "./lib/socket.js";
 
@@ -12,7 +12,7 @@ dotenv.config();
 
 const port = process.env.PORT || 5000;
 const dbUrl = process.env.MONGOURI || "";
-
+const _dirName = path.resolve()
 //middlewares
 app.use(express.json({ limit: '10mb' })) // Increase payload limit to 10MB
 app.use(express.urlencoded({ extended: true, limit: '10mb' }))
@@ -26,6 +26,15 @@ app.use(cors({
 //routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+//if we are in production then server frontend as static 
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(_dirName,"../MingleUp-Frontend/dist")))
+
+  app.get("*",(req,res)=>{
+    res.sendFile(path.join(_dirName,"../MingleUp-Frontend","dist","index.html"))
+  })
+}
 
 server.listen(port, () => {
   console.log(`server is running on port : ${port}`);
